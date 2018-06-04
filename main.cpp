863031ce89b7b071ec10bbe2206a11270537a6ba
Aghiles HAMACHE <aghiles.hamache@etu.utc.fr>
2018-06-04 15:15:05 +0200

    test

--- main.cpp
+++ main.cpp
@@ -2,9 +2,52 @@
 #include <QApplication>
 #include "principal.h"
 
-int main(int argc, char* argv[]) {
-QApplication app(argc, argv);
-Automain fenetre;
-fenetre.show();
-return app.exec();
+int main()
+{
+    /*Automate2D a(2,3,2,4);
+
+    cout << "Creation d'etats" << endl;
+    Etat2D e2(7,4);
+    e2.setCellule(4,2,true);
+    e2.setCellule(3,1,true);
+    e2.setCellule(6,3,true);
+    e2.setCellule(1,0,true);
+    e2setCellule(6,0,true);
+
+    Simulateur2D s(a,e2);
+    cout<<e2;
+    for(unsigned int i=0;i<10;i++){
+
+        s.next();
+        cout<<s.dernier()<<"\n";
+
+    }*/
+	
+	cout<<"Hello World !!"<<std::endl;
+
+    cout << "Creation d'etats" << endl;
+    Etat1D e1(15);
+    e1.setCellule(4,true);
+    e1.setCellule(3,true);
+    e1.setCellule(6,true);
+    e1.setCellule(1,true);
+    e1.setCellule(13,true);
+    e1.setCellule(11,true);
+    e1.setCellule(10,true);
+    e1.setCellule(0,true);
+    e1.setCellule(8,true);
+
+    Automate1D a=Automate1D(25);
+
+    Simulateur1D s(a,e1);
+    cout<<e1;
+    for(unsigned int i=0;i<10;i++){
+        s.next();
+        cout<<s.dernier()<<"\n";
+
+    }
+
+
+    return 0;
+
 }
